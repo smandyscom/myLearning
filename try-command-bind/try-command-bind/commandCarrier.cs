e65e9431fbd995ddeb5bdcb01d7afc0c6327fde3
smandyscom <smandyscom@gmail.com>
2019-03-10 10:33:38 +0800

    wpf command binding and binding techniques

---
+++ try-command-bind/try-command-bind/commandCarrier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace try_command_bind
+{
+    public class commandCarrier
+    {
+        public String Now { get => DateTime.Now.ToString(); }
+
+        /// <summary>
+        /// Return a command instant
+        /// </summary>
+        public ICommand Command { get => new commandImplement(); }
+    }
+}
---
+++ try-command-bind/try-command-bind/commandImplement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Input;
+
+namespace try_command_bind
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class commandImplement : ICommand
+    {
+        public String CommandName { get => GetType().ToString(); }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine(parameter.ToString());
+            
+        }
+    }
+}
