12feea4c1477404e596f38561e4cc0fb7294e2c8
smandyscom <smandyscom@gmail.com>
2019-06-09 23:39:15 +0800

    added word interop

---
+++ try-interop-word/try-interop-word/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Office.Interop.Word;
+
+namespace try_interop_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //open file
+            // Open a doc file.
+            Application application = new Application();
+            Document document = application.Documents.Open("interop_word_trial1.docx");
+
+
+            //grab content/page number
+
+            //output
+        }
+    }
+}
